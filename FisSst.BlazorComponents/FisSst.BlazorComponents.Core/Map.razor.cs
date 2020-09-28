87cd9d606eb653095ab681012df83bdce965c3f8
Marcin Chilicki <mchilicki@fis-sst.pl>
2020-09-28 11:40:51 +0200

    Create empty solution in Visual Studio

---
+++ FisSst.BlazorComponents/FisSst.BlazorComponents.Core/Map.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FisSst.Maps
+{
+    public partial class Map : ComponentBase
+    {
+
+    }
+}
