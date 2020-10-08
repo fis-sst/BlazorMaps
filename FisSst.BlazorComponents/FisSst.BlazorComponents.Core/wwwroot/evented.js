0da9f140219524830110bcb9d2c34d716f629a91
Marcin Chilicki <mchilicki@fis-sst.pl>
2020-10-08 15:29:42 +0200

    Add all mouse events to Evented class and add examples

--- FisSst.BlazorComponents/FisSst.BlazorComponents.Core/wwwroot/evented.js
+++ FisSst.BlazorComponents/FisSst.BlazorComponents.Core/wwwroot/evented.js
@@ -1,6 +1,5 @@
 ﻿export function onCallback(dotnetHelper, evented, eventType) {
     evented.on(eventType, (event) => {
-        console.log(event);
         dotnetHelper.invokeMethodAsync('OnCallback', eventType, {
             type: event.type,
             latLng: event.latlng,
