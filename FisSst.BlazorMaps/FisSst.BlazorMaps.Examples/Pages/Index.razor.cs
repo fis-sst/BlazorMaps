f97a60dc5ea14bce121ab4b4febd5e9744dd35b7
jczerosb <42658793+jczerosb@users.noreply.github.com>
2021-11-23 11:11:38 -0300

    Update Index.razor.cs

--- FisSst.BlazorMaps/FisSst.BlazorMaps.Examples/Pages/Index.razor.cs
+++ FisSst.BlazorMaps/FisSst.BlazorMaps.Examples/Pages/Index.razor.cs
@@ -42,7 +42,7 @@ namespace FisSst.BlazorMaps.Examples.Pages
         }
         
         // Sample to demonstrate getBounds, fitBounds and Rectangle
-        // At end a There will be a zoomed square with 500m radius
+        // Create a zoomed square with 500m radius
         private async Task SetMapInitialView(LatLng pOrigem)
         {
             CircleOptions zDummyOptions = new CircleOptions { Radius = 500, Color = "red" };                 // Set Circle Options
@@ -50,7 +50,7 @@ namespace FisSst.BlazorMaps.Examples.Pages
             LatLngBounds zBounds = await zDummy.GetBounds();                                                 // Get Circle bounds (SW/NE coords)
             await zDummy.Remove();                                                                           // Remove Circle from map
             RectangleOptions zROptions = new RectangleOptions { Color = "blue" };                            // Set Rectangle Options
-            Rectangle retangulo = await this.RectangleFactory.CreateAndAddToMap(zBounds.ToLatLng(), mapRef, zROptions); // Place Rectangle on map using Bounds from Circle
+            Rectangle rectangle = await this.RectangleFactory.CreateAndAddToMap(zBounds.ToLatLng(), mapRef, zROptions); // Place Rectangle on map using Bounds from Circle
             await this.mapRef.FitBounds(zBounds.ToLatLng());                                                 // FitBounds, some king of Zoom All
         }
     }
