4a24914fb5d748d4fc3d9e18e2fd96d8f7a5bd8b
jczerosb <42658793+jczerosb@users.noreply.github.com>
2021-11-23 10:03:38 -0300

    Add files via upload

---
+++ FisSst.BlazorMaps/FisSst.BlazorMaps/Models/Rectangle/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿// Added: Model Class for Rectangle
+
+using FisSst.BlazorMaps.JsInterops.Events;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace FisSst.BlazorMaps
+{
+  /// <summary>
+  /// A rectangle
+  /// </summary>
+  public class Rectangle : Polyline
+  {
+    internal Rectangle(IJSObjectReference jsReference, IEventedJsInterop eventedJsInterop)
+        : base(jsReference, eventedJsInterop)
+    {
+    }
+  }
+}
